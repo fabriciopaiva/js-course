bcc72ff2b25eb94eaa48283ad8965f730c78331a
Fabricio Paiva <fabriciopaiva@gmail.com>
2021-11-12 16:24:30 -0300

    Testing Husk

--- src/main.js
+++ src/main.js
@@ -7,15 +7,14 @@ c = "fdsfdsfdsfddfsdfsf s sdfsdf sdf sdfsdf
 sd sfd fsdfsdfds fsd fsdfs fs fsd sfds fds fsf sfs fs sf sfssfs";
 */
 
-const a = 3
+const a = 3;
 
-const b = 2
+const b = 2;
 
-const c = 'abc'
+const c = 'abc';
 
 function sum() {
   return a + b + c;
 }
-
 
 console.log(sum());
