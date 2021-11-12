771765029acfdff3fbb88b95715907d04ba6f7ff
Fabricio Paiva <fabriciopaiva@gmail.com>
2021-11-12 16:10:31 -0300

    Add Husky

---
+++ src/main.js
@@ -0,0 +1,18 @@
+/*
+a = 3;
+const b = 2
+const b = 4
+
+c = "fdsfdsfdsfddfsdfsf s sdfsdf sdf sdfsdf
+sd sfd fsdfsdfds fsd fsdfs fs fsd sfds fds fsf sfs fs sf sfssfs";
+*/
+
+const a = 3;
+const b = 2;
+const c = 'abc';
+
+function sum() {
+  return a + b + c;
+}
+
+console.log(sum());
