fff6d53af456dce2ecac04ae245979090bb2b112
Fabricio Paiva <fabriciopaiva@gmail.com>
2021-11-12 16:15:40 -0300

    Testing Hook

--- src/main.js
+++ src/main.js
@@ -7,10 +7,8 @@ c = "fdsfdsfdsfddfsdfsf s sdfsdf sdf sdfsdf
 sd sfd fsdfsdfds fsd fsdfs fs fsd sfds fds fsf sfs fs sf sfssfs";
 */
 
-const a = 3;
-
-const b = 2;
-
+const a = 3
+const b = 2
 const c = 'abc'
 
 function sum() {
